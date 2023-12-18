5161a82fe9cb922d52dae52c64a60d80875a7fb9
mohanmohadikar <mohanmmohadikar@gmail.com>
2023-12-18 16:14:35 +0530

    added axios module.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,27 @@
+import axios from 'axios';
+import { BASE_URL } from './constant';
+
 function App() {
-  return <div>React App</div>;
+  const getBlogs = () => {
+    axios
+      .get(`${BASE_URL}/blogs`)
+      .then((res) => {
+        console.log({ res });
+      })
+      .catch((err) => {
+        console.log({ err });
+      });
+  };
+
+  return (
+    <div
+      onClick={() => {
+        getBlogs();
+      }}
+    >
+      React App
+    </div>
+  );
 }
 
 export default App;
